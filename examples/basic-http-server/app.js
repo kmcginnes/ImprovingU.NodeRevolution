8b9a547201a65712e13202e93633bb98610581b0
Kris McGinnes <kmcginnes@me.com>
2015-10-07 21:43:47 -0500

    Turned back into a server

--- examples/basic-http-server/app.js
+++ examples/basic-http-server/app.js
@@ -1,7 +1,12 @@
-var fs = require('fs');
+var http = require('http');
 
-fs.readFile('/', function (err) {
-    if(err) throw err;
-
-    console.log('completed');
+var server = http.createServer((request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    response.end('Hello world');
 });
+
+server.listen(3000);
+
+console.log("Server listening on http://localhost:3000");
